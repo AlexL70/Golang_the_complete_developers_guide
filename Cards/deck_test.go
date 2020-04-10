cda914b482f28eeeb0b754a2bd886d005831c017
AlexL70 <alexander.levinson.70@gmail.com>
2020-04-10 14:40:14 +0300

    32. Writing Useful Tests

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,2 +1,12 @@
 package main
 
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	const dLen = 52
+	d := newDeck()
+
+	if len(d) != dLen {
+		t.Errorf("Expected deck of %d cards but got %d", dLen, len(d))
+	}
+}
