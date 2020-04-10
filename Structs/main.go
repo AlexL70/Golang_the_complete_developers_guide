93e4fb32f7409bbef738f198845096659936a439
AlexL70 <alexander.levinson.70@gmail.com>
2020-04-10 19:56:10 +0300

    40. Embedding Structs

--- Structs/main.go
+++ Structs/main.go
@@ -2,19 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip   int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	// alex := person{
-	// 	firstName: "Alex",
-	// 	lastName: "Levinson"}
-	// fmt.Println(alex)
+	alex := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email: "jim@somemail.com",
+			zip:   9400,
+		},
+	}
 
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Levinson"
 	fmt.Printf("%+v", alex)
 }
